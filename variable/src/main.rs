5f85c29f658a31e3d7678ff65aca3e4ec7033706
wangzhongjie <uvdream@163.com>
2021-08-29 13:24:23 +0000

    feat: 不可变与可变

---
+++ variable/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    println!("x==={}", x);
+    let x = 6;
+    println!("x==={}", x);
+    let mut a=1;
+    println!("a==={}",a);
+    a=2;
+    println!("a==={}",a);
+}
