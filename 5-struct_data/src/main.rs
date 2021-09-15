fff97cc41a15de9a45f44e48e99053f20f37e1f8
wangzhongjie <uvdream@163.com>
2021-09-15 23:02:49 +0800

    feat✨:  更新结构体

--- 5-struct_data/src/main.rs
+++ 5-struct_data/src/main.rs
@@ -18,8 +18,23 @@ fn main() {
     println!("{:#?}", user1);
     let new_user = build_user(user1.email, user1.name);
     println!("{:#?}", new_user);
+    // 3.结构体更新语法
+    let user2 = User {
+        email: String::from("another@example.com"),
+        name: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+    // 简介写法
+    let user3 = User {
+        email: String::from("another@example.com"),
+        name: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("更新结构体");
+    println!("{:#?}--{:#?}", user2, user3)
 }
-// 复用值
+// 1.复用值
 fn build_user(email: String, name: String) -> User {
     // User {
     //     email: email,
@@ -27,7 +42,7 @@ fn build_user(email: String, name: String) -> User {
     //     active: true,
     //     sign_in_count: 2,
     // }
-    // 简介写法
+    // 2.简介写法
     User {
         email,
         name,
