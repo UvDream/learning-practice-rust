998d28fca873fdc40372c4d7090667b05e7c4fcb
wangzhongjie <wangzj@cyberpeace.cn>
2021-10-08 18:00:03 +0800

    feat:  string

--- 8-string/src/main.rs
+++ 8-string/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    update_string()
+}
+fn update_string() {
+    let mut s = String::from("哈哈");
+    // println!("{}", &s);
+    // 更新字符串
+    s.push_str("嘻嘻");
+    println!("{}", &s);
+
+    // push 方法被定义为获取一个单独的字符作为参数
+    s.push('1');
+    println!("{}", &s);
+    // 拼接字符串
+    let s1 = String::from("这是s2");
+    let s2 = s + &s1;
+    println!("{}", &s2);
 }
